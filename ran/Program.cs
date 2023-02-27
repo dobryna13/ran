02c0de330894436df9aed97fe30e3cb445e4dcf7
dobryna13 <mdobrinchuk@gmail.com>
2023-02-27 21:44:29 +0200

    Добавьте файлы проекта.

---
+++ ran/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 5;
+int b = 10;
+int c = 15;
+
+// Створення анонімного методу, який приймає три цілі аргументи та повертає їх середнє арифметичне
+Func<int, int, int, double> average = delegate (int x, int y, int z) {
+    return (x + y + z) / 3.0;
+};
+
+double result = average(a, b, c);
+Console.WriteLine("Середнє арифметичне: {0}", result);
+
